1dccc6b48ab45bbf5f00e2611bca8c22e260d945
tdheconstant10-design <tdheconstant10@gmail.com>
2026-07-25 09:42:46 +0000

    Create Assignment1.java
    Initial commit - assignment upload

---
+++ Try1/src/Assignment1.java
@@ -0,0 +1,25 @@
+public class Assignment1 {
+    public static void main(String[] args) {
+
+        //Numbers
+        int N1 = 7;
+        int N2 = 5;
+        int N3 = 2;
+
+
+        //Addition, Subtraction, Multiplication, Division and Average
+        int sum = N1 + N2 + N3;
+        int diff = N1 - N2 - N3;
+        int product = N1 * N2 * N3;
+        float quotient = (float) N1 / N2 / N3;
+        float Average = (float) sum / 3;
+
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(product);
+        System.out.println(quotient);
+        System.out.println(Average);
+
+    }
+}
